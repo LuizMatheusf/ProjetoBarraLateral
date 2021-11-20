baa510826b1081111033da66facbfb74cdad20c9
LuizMatheusf <93159542+LuizMatheusf@users.noreply.github.com>
2021-11-20 10:12:04 -0300

    Meu primeiro Commit

---
+++ SideBar.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('#menu');
+let sidebar = document.querySelector('.sidebar');
+let search = document.querySelector('.bx-search');
+let conteudo = document.querySelector('.conteudo');
+
+menu.onclick = function(){
+    sidebar.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+}
+
+search.onclick = function(){
+    sidebar.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+}
