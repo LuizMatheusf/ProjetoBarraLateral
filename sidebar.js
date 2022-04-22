fb775e70451683b4bf3e8fc48ec705b99825d38f
Luiz Matheus F. Dos Santos <93159542+LuizMatheusf@users.noreply.github.com>
2022-04-22 19:16:09 -0300

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,13 +1,30 @@
-let menu = document.querySelector('#menu');
-let sidebar = document.querySelector('.sidebar');
-let search = document.querySelector('.bx-search');
-let conteudo = document.querySelector('.conteudo');
-
-menu.onclick = function(){
-    sidebar.classList.toggle('ativo')
-    conteudo.classList.toggle('ativo')
-}
-search.onclick = function(){
-    sidebar.classList.toggle('ativo')
-    conteudo.classList.toggle('ativo')
-}
+let body = document.querySelector('body'),
+    menu = document.querySelector('#menu'),
+    sidebar = document.querySelector('.sidebar'),
+    search = document.querySelector('.bx-search'),
+    conteudo = document.querySelector('.content'),
+    theme = document.querySelector('.theme'),
+    moon_sun = document.querySelector('.moon_sun'),
+    toggle = document.querySelector('.switch');
+    
+    menu.onclick = function(){
+        sidebar.classList.toggle('active')
+        conteudo.classList.toggle('active')
+    }
+    
+    search.onclick = function(){
+        sidebar.classList.toggle('active')
+        conteudo.classList.toggle('active')
+    }
+    
+    toggle.onclick = function(){ 
+        body.classList.toggle('dark')
+    }    
+    
+    theme.onclick = function(){ 
+        body.classList.toggle('dark')
+    }    
+    
+    moon_sun.onclick = function(){ 
+        body.classList.toggle('dark')
+    }    
